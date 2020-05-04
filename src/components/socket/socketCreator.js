b6b626fdbffbb67c52834b3ecf21d85f63485834
Aditya Acharya <aditya.acharya@objectedge.com>
2020-05-04 16:22:50 -0700

    Socket code

---
+++ src/components/socket/socketCreator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import express from 'express';
+import socketIo from ' '
+
+class SocketCreator extends React.Component {
+
+    constructor () {
+        super();
+        this.state ={};
+      }     
+    render() {
+        return (
+            <div>
+            </div>
+        ); 
+    }
+}
+export default SocketCreator;
